6b382fd775a9b8df5ec9efeab00617c8a31d5ebf
Nahu Merawi <nbetsiha@gmail.com>
2020-11-03 20:58:19 -0600

    Updated Kafka config

--- src/main/java/com/pm/paymentgateway/configuration/KafkaConfig.java
+++ src/main/java/com/pm/paymentgateway/configuration/KafkaConfig.java
@@ -22,53 +22,53 @@ public class KafkaConfig extends JsonDeserializer{
 
 
 
-    @Bean
-    public ConsumerFactory<String, String> consumerFactory(){
-
-        Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        config.put(ConsumerConfig.GROUP_ID_CONFIG,"group_id");
-        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-
-        return new DefaultKafkaConsumerFactory<>(config);
-    }
-
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> concurrentKafkaListenerContainerFactory (){
-
-        ConcurrentKafkaListenerContainerFactory<String, String> factory =
-                new ConcurrentKafkaListenerContainerFactory<String, String>();
-
-        factory.setConsumerFactory(consumerFactory());
-        return factory;
-    }
-
-    // Factory for model Order
-    @Bean
-    public ConsumerFactory<String, Order> orderConsumerFactory(){
-
-        Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "ec2-18-207-140-130.compute-1.amazonaws.com:9092");
-        config.put(ConsumerConfig.GROUP_ID_CONFIG,"order");
-        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        config.put(ConsumerConfig.SECURITY_PROVIDERS_CONFIG, "*");
-
-        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(),
-                new JsonDeserializer<>(Order.class));
-    }
-
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Order> orderKafkaListenerFactory(){
-
-        ConcurrentKafkaListenerContainerFactory<String, Order> factory = new
-                ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(orderConsumerFactory());
-
-        return factory;
-    }
+//    @Bean
+//    public ConsumerFactory<String, String> consumerFactory(){
+//
+//        Map<String, Object> config = new HashMap<>();
+//        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+//        config.put(ConsumerConfig.GROUP_ID_CONFIG,"group_id");
+//        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+//        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+//
+//        return new DefaultKafkaConsumerFactory<>(config);
+//    }
+//
+//    @Bean
+//    public ConcurrentKafkaListenerContainerFactory<String, String> concurrentKafkaListenerContainerFactory (){
+//
+//        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+//                new ConcurrentKafkaListenerContainerFactory<String, String>();
+//
+//        factory.setConsumerFactory(consumerFactory());
+//        return factory;
+//    }
+//
+//    // Factory for model Order
+//    @Bean
+//    public ConsumerFactory<String, Order> orderConsumerFactory(){
+//
+//        Map<String, Object> config = new HashMap<>();
+//        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "ec2-18-207-140-130.compute-1.amazonaws.com:9092");
+//        config.put(ConsumerConfig.GROUP_ID_CONFIG,"order");
+//        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+//        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+//        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+//        config.put(ConsumerConfig.SECURITY_PROVIDERS_CONFIG, "*");
+//
+//        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(),
+//                new JsonDeserializer<>(Order.class));
+//    }
+//
+//    @Bean
+//    public ConcurrentKafkaListenerContainerFactory<String, Order> orderKafkaListenerFactory(){
+//
+//        ConcurrentKafkaListenerContainerFactory<String, Order> factory = new
+//                ConcurrentKafkaListenerContainerFactory<>();
+//        factory.setConsumerFactory(orderConsumerFactory());
+//
+//        return factory;
+//    }
 
 
 
@@ -83,6 +83,7 @@ public class KafkaConfig extends JsonDeserializer{
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
 
+//        JsonDeserializer<ProductDto> jsonDeserializer = new JsonDeserializer<>(ProductDto.class);
 
         return new DefaultKafkaProducerFactory<>(config);
     }
